88cb801d4d745ebaecf8bf7ebcdd17595cbdc72e
Casandra Silva <silvacasandra@gmail.com>
2019-08-16 12:08:03 -0500

    create recipe and user models and migrations

---
+++ models/recipe.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Recipe = sequelize.define('Recipe', {
+    title: DataTypes.STRING,
+    ingredients: DataTypes.TEXT,
+    direction: DataTypes.TEXT
+  }, {});
+  Recipe.associate = function(models) {
+    // associations can be defined here
+  };
+  return Recipe;
+};
