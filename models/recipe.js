4e5e49c31f8ea58d0d14ac9bece7659def57a879
Casandra Silva <silvacasandra@gmail.com>
2019-08-20 23:46:39 -0400

    add user and recipe associations

--- models/recipe.js
+++ models/recipe.js
@@ -7,6 +7,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {});
   Recipe.associate = function(models) {
     // associations can be defined here
+    Recipe.belongsTo(models.User, { foreignKey: 'userId' })
   };
   return Recipe;
 };
